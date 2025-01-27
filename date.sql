7532a2235ba3dfdb28559c78be36b5519d85286b
ariful <arifulislam321054@gmail.com>
2025-01-27 18:19:32 +0600

    age, interval, extract

--- date.sql
+++ date.sql
@@ -7,6 +7,15 @@ SELECT now()::date;
 SELECT current_date; 
 
 -- current time without date  
-SELECT now()::time;
-SELECT current_time; -- without time zone  
+SELECT now()::time; -- without time zone
+SELECT current_time;   
+
+SELECT to_char(now(), 'DD/MM/YYYY');
+
+SELECT age(current_timestamp, '2015-01-01');
+
+SELECT current_timestamp - '2015-01-01'::DATE;
  
+ SELECT current_timestamp - INTERVAL '1 year'
+
+ SELECT extract(month from '2015-05-01'::date);
